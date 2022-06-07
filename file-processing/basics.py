0337a04dc525a045524805dfc466afe259f09175
GersonGmd <gersondiketama@yahoo.com>
2022-06-07 12:06:52 -0500

    added files to file processing

--- file-processing/basics.py
+++ file-processing/basics.py
@@ -1,12 +1,11 @@
-from asyncore import read
 
 
-myfile = open("fruits.txt")
+myfile = open("files/fruits.txt")
 print(myfile.read())
 
 # this is if I want to read the content many times
 
-file = open("fruits.txt")
+file = open("files/fruits.txt")
 content = file.read()
 
 print(content)
@@ -16,8 +15,35 @@ print(content)
 
 # better way to read file
 
-with open("fruits.txt") as myfiles:
+with open("files/fruits.txt") as myfiles:
     # this will read and close it
     contents = myfiles.read()
+
+print(content)
+
+
+# Writing into a file
+
+with open("files/vegetables.txt", "w") as writfile:
+    cont = writfile.write("Tomato\nCucumber\nOnion\Gerson")
+
+print(cont)
+
+
+# copying 90 charathers from another file to past into different file.
+
+with open("bear.txt") as myfile:
+    content = myfile.read()
+    chart = content[:90]
+    with open("first.txt", "w") as newfile:
+        cont = newfile.write(chart)
+
+
+# Appending to a text file
+
+with open("files/vegetables.txt", "a+") as myfile:
+    myfile.write("\nOkra")
+    myfile.seek(0)
+    content = myfile.read()
 
 print(content)
---
+++ modules/basics.py
@@ -0,0 +1,5 @@
+
+with open("tomato.txt", "w") as contfile:
+    cont = contfile.write("romato")
+    readeable = contfile.read()
+print(readeable)
